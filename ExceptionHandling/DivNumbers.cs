24f9d88d0b881e035d1281a2fe40281a5ed86e44
VMC Cuties <VMC Cuties@DESKTOP-JSF71KL>
2017-11-22 21:11:58 +1100

    Change again

--- ExceptionHandling/DivNumbers.cs
+++ ExceptionHandling/DivNumbers.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// This is exception Handling program
+/// </summary>
 namespace ExceptionHandling
 {
+    /// <summary>
+    /// this is the public class div Numbers
+    /// </summary>
     public class DivNumbers
     {
+        /// <summary>
+        /// this is an integer variable result.
+        /// </summary>
         int result;
+        /// <summary>
+        /// this is the constructor
+        /// </summary>
         public DivNumbers()
-        {
+        {            
             result = 0;
         }
-        public void division(int num1, int num2)
-        {
+        /// <summary>
+        /// this is a method called Division
+        /// </summary>
+        /// <param name="num1">This is the first parameter</param>
+        /// <param name="num2">This is the second parameter</param>
+     public void division(int num1, int num2)
+        {            
             try
-            {
+            {                
                 result = num1 / num2;
             }
             catch (DivideByZeroException e)
